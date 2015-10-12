b0bbc6348a21fff325e2891ed620aa71d169d610
ishchat <ishan.chaturvedi@gmail.com>
2015-10-12 16:21:15 +0530

    Update curl_fun.h

--- src/curl_fun.h
+++ src/curl_fun.h
@@ -5,6 +5,11 @@
 #include <jansson.h>
 #include <curl/curl.h>
 
+//jansson is C library for working with JSON data
+
+/*In computer programming, a callback is a piece of executable code that is passed as an argument to other code, 
+which is expected to call back (execute) the argument at some convenient time.
+A callback method is one which is passed as an argument in another method and which is invoked after some kind of event. The 'call back' nature of the argument is that, once its parent method completes, the function which this argument represents is then called; that is to say that the parent method 'calls back' and executes the method provided as an argument.*/
 // general curl callback
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
 
